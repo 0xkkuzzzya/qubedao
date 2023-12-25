49e164feb804c83093182b01768c68c262987e7f
0xkkuzzzya <130092860+0xkkuzzzya@users.noreply.github.com>
2023-12-26 01:57:57 +0500

    fix:

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,122 @@
 import styled from "styled-components";
+import { ListFormat } from "typescript";
 
 const MainFooter = styled.div`
     width: 100%;
-    height: 250px;
+    max-height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 30px 0px;
+    @media (max-width: 840px) {
+        flex-direction: column;
+    }
+`
+
+const LinkBlock = styled.div`
+    padding-left: 40px;
+    padding-right: 40px;
+    text-align: left;
+    @media (max-width: 840px) {
+        padding-top: 25px;
+        width: 100%;
+    }
+    @media (max-width: 450px) {
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+`
+
+const NameBlock = styled.div`
+    font-size: 25px;
+    font-weight: 700;
+    margin-bottom: 10px;
+    @media (max-width: 450px) {
+        font-size: 22px;
+    }
+`
+
+const LinkText = styled.a`
+    font-weight: 700;
+    font-size: 18px;
+    color: #333;
+    @media (max-width: 450px) {
+        font-size: 16px;
+    }
+`
+
+const Link = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    grid-gap: 10px;
+`
+
+const FirstBlock = styled.div`
+    display: flex;
+    justify-content: center;
+    @media (max-width: 840px) {
+        width: 100%;
+    }
+`
+
+const SecondBlock = styled.div`
+    display: flex;
+    justify-content: center;
+    @media (max-width: 840px) {
+        width: 100%;
+    }
+    
 `
 
 
 
 export const Footer = () => {
+
+    let Ecosystem = <NameBlock>Ecosystem</NameBlock>
+    let Network = <NameBlock>Network</NameBlock>
+    let Learn = <NameBlock>Learn</NameBlock>
+    let Community = <NameBlock>Community</NameBlock>
+
     return(
-        <MainFooter></MainFooter>
+        <MainFooter>
+            <FirstBlock>
+                <LinkBlock>
+                    {Ecosystem}
+                    <Link>
+                        <LinkText>Get a wallet</LinkText>
+                        <LinkText>Savings</LinkText>
+                        <LinkText>Stablecoins</LinkText>
+                        <LinkText>Exchange</LinkText>
+                        <LinkText>Bridge</LinkText>
+                    </Link>
+                </LinkBlock>
+            <LinkBlock>
+                {Network}
+                <Link>
+                    <LinkText>Explorer</LinkText>
+                    <LinkText>Tech Documentation</LinkText>
+                    <LinkText>Github</LinkText>
+                </Link>
+            </LinkBlock>
+            </FirstBlock>
+            <SecondBlock>
+                <LinkBlock>
+                    {Learn}
+                    <Link>
+                        <LinkText>Blog</LinkText>
+                        <LinkText>Documentation</LinkText>
+                    </Link>
+                </LinkBlock>
+                <LinkBlock>
+                    {Community}
+                    <Link>
+                        <LinkText>Telegram</LinkText>
+                        <LinkText>X.com</LinkText>
+                        <LinkText>Discord</LinkText>
+                    </Link>
+                </LinkBlock>
+            </SecondBlock>
+        </MainFooter>
     ) 
 }
--- src/components/FourthBlock/FourthBlock.tsx
+++ src/components/FourthBlock/FourthBlock.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Telegram from '../../assets/svg/Telegram.svg'
 
 const MainBlock = styled.div`
     width: 100%;
@@ -8,19 +9,106 @@ const FirstBlock = styled.div`
     max-width: 100%;
     height: 350px;
     background-color: #66BAFF;
+    text-align: center;
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 20px;
+`
+
+const NameText = styled.h1`
+    padding-top: 30px;
+    font-size: 55px;
+    color: white;
+    margin-top: 20px;
+    @media (max-width: 720px) {
+        font-size: 40px;
+        margin-top: 10px;
+    }
+`
+
+const UnderText = styled.h1`
+    max-width: 50%;
+    font-size: 25px;
+    margin-top: 35px;
+    color: white;
+    @media (max-width: 720px) {
+        font-size: 18px;
+        margin-top: 20px;
+    }
 `
 
 const SecondBlock = styled.div`
     max-width: 100%;
     height: 350px;
     background-color: #474747;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 20px;
+`
+
+const FirstButton = styled.button`
+    width: 200px;
+    height: 50px;
+    border: 2px solid white;
+    background-color: transparent;
+    border-radius: 50px;
+    font-size: 16px;
+    font-family: 'Metropolis', sans-serif;
+    font-weight: 600;
+    color: white;
+    cursor: pointer;
+    margin-top: 20px;
+    @media (max-width: 720px) {
+        margin-top: 30px;
+    }
+`
+
+const SecondButton = styled.button`
+    width: 200px;
+    height: 50px;
+    border: none;
+    background-color: #46ACFF;
+    font-size: 16px;
+    font-family: 'Metropolis', sans-serif;
+    border-radius: 50px;
+    color: white;
+    font-weight: 600;
+    cursor: pointer;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    @media (max-width: 720px) {
+        margin-top: 30px;
+    }
+`
+
+const TelegramImg = styled.img`
+    width: 30px;
+    height: 30px;
 `
 
 export const FouthBlock = () => {
     return(
         <MainBlock>
-            <FirstBlock></FirstBlock>
-            <SecondBlock></SecondBlock>
+            <FirstBlock>
+                <NameText>Developer resources</NameText>
+                <UnderText>Everything you need to get started building on Qube, all in one place.</UnderText>
+                <FirstButton>Start building</FirstButton>
+            </FirstBlock>
+            <SecondBlock>
+                <NameText>Join the community</NameText>
+                <UnderText>Join a vibrant community of innovators and builders creating a more open web.</UnderText>
+                <SecondButton>
+                    English channel
+                    <TelegramImg src={Telegram}></TelegramImg>
+                </SecondButton>
+            </SecondBlock>
         </MainBlock>
     ) 
 }
